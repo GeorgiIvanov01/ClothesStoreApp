1cbcd397c3b349dd69080803f540caef3f5297ab
GeorgiIvanov01 <satela8909@gmail.com>
2026-02-19 15:07:55 +0000

    RequireConfirmedAccount changed to fals as we dont want email confirmation.

--- ClothesStoreApp/Program.cs
+++ ClothesStoreApp/Program.cs
@@ -11,7 +11,7 @@ builder.Services.AddDbContext<ClothesStoreDbContext>(options =>
     options.UseSqlServer(connectionString));
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
-builder.Services.AddDefaultIdentity<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = true)
+builder.Services.AddDefaultIdentity<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = false)
     .AddEntityFrameworkStores<ClothesStoreDbContext>();
 builder.Services.AddControllersWithViews();
 
